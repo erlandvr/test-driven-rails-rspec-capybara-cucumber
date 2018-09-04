3c1419cacd5d9343eb600cf3b1c46898b6e95c4b
Erland Van Reet <erlandvr@gmail.com>
2018-09-04 13:56:29 +0200

    summary: what have we learned from our first kata?

---
+++ test-driven-rails-rspec-capybara-cucumber/zelf-written-notes/4-bowling-game-kata-lessons-learned.rb
@@ -0,0 +1,15 @@
+note: 'the most difficult thing is to start!'
+
+Tips:
+  - start from the easiest example
+    * clean slate state
+      vb: default values || obj. init. || page with empty dataset etc.
+  - add new examples and extend functionality
+  - refactor
+    * start and finish in 'GREEN'
+  - keep examples small with one assertion/example
+  - readability and clearness of tests is utmost important
+    *tests do not get tested!
+
+
+
